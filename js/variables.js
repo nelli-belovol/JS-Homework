d5101619dc0cbbe412842b03ee1781238bd43d75
nelli-belovol <nelli199020@gmail.com>
2021-06-15 17:49:05 +0300

    add variables.js

---
+++ js/variables.js
@@ -0,0 +1,25 @@
+console.log (student);//undefined
+
+var student = "Nelli";
+console.log (student);
+
+var count;
+console.log(count); //undefined
+
+// console.log (number); // module-1-1.js:281 Uncaught ReferenceError: Cannot access 'number' before initialization
+let number = 1;
+console.log (number);
+
+let age;
+console.log (age); //undefined
+age= 18;
+console.log (age);
+age= 22;
+console.log (age);
+
+
+// console.log (userName); // module-1-1.js:285 Uncaught ReferenceError: Cannot access 'userName' before initialization
+const userName = "Vika"
+console.log (userName);
+
+//const user; // Uncaught SyntaxError: Missing initializer in const declaration
